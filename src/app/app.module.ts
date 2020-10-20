2c4d5bc7c36f3099780e05443dc856ee713987d4
MSCINSYSTEMS-PC\MSC IN SYSTEMS <Ruben>
2020-10-19 21:29:49 -0430

    Creacion de Componentes Configuracion de Rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,70 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './components/app/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDividerModule} from '@angular/material/divider';
+import { ProfileComponent } from './components/profile/profile.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DashboardUsersComponent } from './components/dashboard-users/dashboard-users.component';
+import { HeaderComponent } from './components/header/header.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+
+const rutas: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'inicio'
+  },
+  {
+    path: 'inicio',
+    component: InicioComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      /* {
+        path: '',
+        pathMatch: 'prefix',
+        redirectTo: 'users'
+      }, */
+      {
+        path: 'users',
+        component: DashboardUsersComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    DashboardComponent,
+    HeaderComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    RouterModule
+    RouterModule.forRoot(rutas,
+      {
+        //Parametros Basicos
+        //Sirve para ver comportamiento por el Console
+        enableTracing: true,
+        //Para que el Hijo Herede del Padre
+        paramsInheritanceStrategy: "always",
+        //Hace que entienda la URL como Ruta y NO como Carpeta
+        useHash: true
+      }
+      ),
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
